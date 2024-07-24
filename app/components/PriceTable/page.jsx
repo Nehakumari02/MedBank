f5d9ff6877fa98f86400bdd4d66e46f574d5daa7
Neha Kumari <b20115@students.iitmandi.ac.in>
2024-07-24 10:57:57 +0530

    homepage-offerings-section

--- app/components/PriceTable/page.jsx
+++ app/components/PriceTable/page.jsx
@@ -1,52 +1,72 @@
+import HomePageSectionHeader from "../HomePageSectionHeader/page";
+
 const PriceTable = () => (
-    <section className="p-6 bg-white rounded-lg shadow-md mb-6">
-      <h2 className="text-2xl font-bold text-blue-600 mb-4">REFERENCE PRICE</h2>
-      <h3 className="text-xl font-semibold mb-4">PRICE TABLE</h3>
-      <p className="mb-4">
+  <>
+    <section className="px-[62px] py-[100px] w-full text-[#333333]">
+        <HomePageSectionHeader title={"Reference Price"}/>
+        <div className='pl-[90px] pt-[20px] w-full flex flex-col items-start gap-[32px]'>
+        <span className="flex items-start justify-start gap-[12px] font-sans text-[36px] font-medium leading-[42px] text-[#60B7CF]">PRICE TABLE {ArrowRightIcon}</span>
+        <p className="font-sans font-normal text-[20px] leading-[34px]">
         "Explore our reference prices to gain insight into the cost structure of our genetic analysis services. While actual prices may vary depending on specific project requirements, our reference prices provide an estimate of the costs involved."
-      </p>
-      <table className="w-full border-collapse">
-        <thead>
-          <tr>
-            <th className="border p-2">Plan</th>
-            <th className="border p-2">Read Length</th>
-            <th className="border p-2">Data Volume</th>
-            <th className="border p-2">Reference Price</th>
-            <th className="border p-2">Delivery Time</th>
+        </p>
+        <div className="w-full">
+        <table className="w-full">
+        <thead className="">
+          <tr className="bg-[#4DBED8] rounded-tl-[10px] rounded-tr-[10px] h-[80px] flex items-center justify-between px-[20px]">
+            <th className="">Read Length</th>
+            <th className="">Plan</th>
+            <th className="">Data Volume</th>
+            <th className="">Reference Price</th>
+            <th className="">Delivery Time</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="w-full">
           <tr>
-            <td className="border p-2">DNA Sequencing</td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
+            <td className="">DNA Sequencing</td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
           </tr>
           <tr>
-            <td className="border p-2">RNA Sequencing</td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
+            <td className="">RNA Sequencing</td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
           </tr>
           <tr>
-            <td className="border p-2">Amplicon Sequencing</td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
+            <td className="">Amplicon Sequencing</td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
           </tr>
           <tr>
-            <td className="border p-2">Human Genome Sequencing</td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
-            <td className="border p-2"></td>
+            <td className="">Human Genome Sequencing</td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
+            <td className=""></td>
           </tr>
         </tbody>
       </table>
+        </div>
+    </div>
     </section>
+  </>
+    
   );
   
   export default PriceTable;
+
+  const ArrowRightIcon = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.33 18.794C12.541 19.0047 12.8269 19.123 13.125 19.123C13.4231 19.123 13.7091 19.0047 13.92 18.794L20.295 12.419C20.5057 12.2081 20.624 11.9222 20.624 11.624C20.624 11.3259 20.5057 11.04 20.295 10.829L13.92 4.45404C13.7068 4.25507 13.4246 4.14668 13.1331 4.15172C12.8415 4.15676 12.5632 4.27483 12.357 4.48104C12.1508 4.68726 12.0327 4.96549 12.0277 5.25708C12.0227 5.54867 12.131 5.83082 12.33 6.04404L16.785 10.499H5.62501C5.32664 10.499 5.0405 10.6176 4.82952 10.8285C4.61854 11.0395 4.50001 11.3257 4.50001 11.624C4.50001 11.9224 4.61854 12.2086 4.82952 12.4195C5.0405 12.6305 5.32664 12.749 5.62501 12.749H16.785L12.33 17.204C12.1193 17.415 12.001 17.7009 12.001 17.999C12.001 18.2972 12.1193 18.5831 12.33 18.794Z" fill="url(#paint0_linear_277_1151)"/>
+<defs>
+<linearGradient id="paint0_linear_277_1151" x1="20.624" y1="11.6373" x2="4.50001" y2="11.6373" gradientUnits="userSpaceOnUse">
+<stop offset="0.1" stopColor="#60B7CF"/>
+<stop offset="0.745" stopColor="#3E8DA7"/>
+<stop offset="1" stopColor="#003E5C" stopOpacity="0.6"/>
+</linearGradient>
+</defs>
+</svg>
