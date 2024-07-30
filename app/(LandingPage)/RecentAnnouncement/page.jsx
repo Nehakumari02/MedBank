ef568a483c3b9f3f69066aa0a14de91b052086b3
Nehapanwal <nehapanwal02@gmail.com>
2024-07-30 17:36:50 +0530

    7th commit

--- app/(LandingPage)/RecentAnnouncement/page.jsx
+++ app/(LandingPage)/RecentAnnouncement/page.jsx
@@ -10,8 +10,6 @@ import article2 from '../../../public/Images/Home/article2.png'
 import article3 from '../../../public/Images/Home/article3.png'
 import ArticleCard from '../../../components/ArticleCard'
 
-
-
 const articles = [
   {
     image: article1.src,
@@ -25,7 +23,6 @@ const articles = [
     date: '24-july-2024 12:45',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
-
   {
     image: article3.src,
     title: 'Title',
@@ -34,82 +31,66 @@ const articles = [
   }
 ];
 
-
-const page = () => {
+const Page = () => {
   return (
     <>
-      <section className='w-100vw text-#333333 font-sans flex flex-col items-center justify-center py-[60px]'>
-        <div className='w-[1028px] flex flex-col items-start  gap-[39px] '>
-          <div className='font-bold text-[32px] leading-[24px]'>
+      <section className='max-w-screen-lg mx-auto text-[#333333] font-DM-Sans flex flex-col items-center justify-center py-[60px] px-4'>
+        
+        <div className='w-full flex flex-col items-start gap-[39px]'>
+        <div className='text-[12px] sm:text-[18px]'>Date : 12 JUNE 2024</div>
+          <div className='font-bold text-[16px] sm:text-[28px] md:text-[32px] leading-[24px]'>
             Article or post title
           </div >
 
-          <div className='font-normal text-[24px] leading-[36px]'>
+          <div className='font-normal text-[12px] sm:text-[18px] md:text-[24px] leading-[36px]'>
             Subheading that sets up context, shares more info about the author, or generally gets people psyched to keep reading
-            <br></br>
-            <br></br>
+            <br /><br />
 
-            <Image src={Announcement1} alt='Announcement1'></Image>
+            <Image src={Announcement1} alt='Announcement1' />
           </div>
-          <div className='font-normal text-[20px] leading-[30px]'>
-            Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look: <br></br> <br></br>
+          <div className='font-normal text-[12px] sm:text-[18px] md:text-[20px] leading-[30px]'>
+            Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look: <br /><br />
 
-            Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud
-            nisi intricate Content. Qui  international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod
-            irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui
-            wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur. <br></br> <br></br>
+            Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur. <br /><br />
 
-            Exquisite sophisticated iconic cutting-edge laborum deserunt Addis Ababa esse bureaux cupidatat id minim. Sharp classic
-            the best commodo nostrud delightful. Conversation aute Rochester id. Qui sunt remarkable deserunt intricate airport handsome K-pop
-            excepteur classic esse Asia-Pacific laboris. <br></br> <br></br>
+            Exquisite sophisticated iconic cutting-edge laborum deserunt Addis Ababa esse bureaux cupidatat id minim. Sharp classic the best commodo nostrud delightful. Conversation aute Rochester id. Qui sunt remarkable deserunt intricate airport handsome K-pop excepteur classic esse Asia-Pacific laboris. <br /><br />
           </div>
-          <div className='flex '>
+          <div className='flex'>
             <div className='pr-[40px]'>
-              <Image src={Announcement2} alt='Announcement2'></Image>
+              <Image src={Announcement2} alt='Announcement2' />
             </div>
             <div>
-
-              <Image src={Announcement3} alt='Announcement3'></Image>
+              <Image src={Announcement3} alt='Announcement3' />
             </div>
-
           </div>
-          <div className='font-normal text-[20px] leading-[30px]'>
-            Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look:<br></br><br></br>
+          <div className='font-normal text-[12px] sm:text-[18px] md:text-[20px] leading-[30px]'>
+            Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look:<br /><br />
 
-            Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect
-            nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual
-            adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality
-            of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia
-            Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur. <br></br><br></br>
+            Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur. <br /><br />
 
-            Exquisite sophisticated iconic cutting-edge laborum deserunt Addis Ababa esse bureaux cupidatat id minim. Sharp
-            classic the best commodo nostrud delightful. Conversation aute Rochester id. Qui sunt remarkable deserunt intricate
-            airport handsome K-pop excepteur classic esse Asia-Pacific laboris.
+            Exquisite sophisticated iconic cutting-edge laborum deserunt Addis Ababa esse bureaux cupidatat id minim. Sharp classic the best commodo nostrud delightful. Conversation aute Rochester id. Qui sunt remarkable deserunt intricate airport handsome K-pop excepteur classic esse Asia-Pacific laboris.
           </div>
-          <div className='font-bold text-[32px] leading-[35.2px]'>
+          <div className='font-bold text-[16px] sm:text-[32px] leading-[35.2px]'>
             Related article or posts
           </div>
-
         </div>
       </section>
       <div className="container mx-auto p-4 flex items-center justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {articles.map((article, index) => (
-                <ArticleCard
-                  key={index}
-                  image={article.image}
-                  title={article.title}
-                  date={article.date}
-                  description={article.description}
-                />
-              ))}
-            </div>
-          
-          </div>
-
-      {/* <Footer></Footer> */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {articles.map((article, index) => (
+            <ArticleCard
+              key={index}
+              image={article.image}
+              title={article.title}
+              date={article.date}
+              description={article.description}
+            />
+          ))}
+        </div>
+      </div>
     </>
   )
 }
 
-export default page
+export default Page;
+
--- app/(LandingPage)/SitePolicy/page.jsx
+++ app/(LandingPage)/SitePolicy/page.jsx
@@ -3,105 +3,96 @@ import React from 'react'
 const page = () => {
     return (
         <>
-            <section className=" w-[100vw]  flex flex-col items-center justify-center text-[#333333]">
-                <div className="w-[1032px]  py-[60px]  flex flex-col items-start gap-[24px]">
-                    <h2 className="w-[161px] leading-[24px] font-sans text-[32px] font-bold">Site Policy</h2>
-                    <span className='font-normal font-sans text-[24px] leading-36px'>Customers using the website (https://medbank.sg/) provided by Medbank (hereinafter
-                        referred to as “we” or “us”) are requested to fully read the following.</span>
+            <section className="w-full flex flex-col items-center justify-center text-[#333333] px-4">
+                <div className="max-w-screen-lg w-full pt-[60px] flex flex-col items-start gap-[24px]">
+                    <h2 className="text-center leading-[24px] font-DM-Sans text-[16px] sm:text-[32px] font-bold">
+                        Site Policy
+                    </h2>
+                    <span className='font-normal font-DM-Sans text-[12px] sm:text-[24px] leading-36px'>
+                        Customers using the website (https://medbank.sg/) provided by Medbank (hereinafter
+                        referred to as “we” or “us”) are requested to fully read the following.
+                    </span>
                     <div>
-                        <div className="font-sans font-medium text-[24px] leading-36px">
+                        <div className="font-DM-Sans font-medium text-[24px] leading-36px">
                             ・Disclaimer
-
                         </div>
-                        <span className='font-normal font-sans text-[24px] leading-36px'>
-                            Although we takes the utmost care in posting information on the Medbank's website,
-                            Medbank does not guarantee the accuracy or completeness of this information. <br></br>
+                        <span className='font-normal font-DM-Sans text-[12px] sm:text-[24px] leading-36px'>
+                            Although we take the utmost care in posting information on Medbank's website,
+                            Medbank does not guarantee the accuracy or completeness of this information. <br />
                             Medbank reserves the right to change the information on its website at any time without
-                            notice. <br></br>
+                            notice. <br />
                             Medbank and its affiliates shall not be liable for any direct or indirect loss incurred by you as
-                            a result of your use of the information or content contained in the Medbank Web site.
+                            a result of your use of the information or content contained in the Medbank website.
                         </span>
                     </div>
                     <div>
-
-                        <div className="font-sans font-medium text-[24px] leading-36px">
+                        <div className="font-DM-Sans font-medium text-[12px] sm:text-[24px] leading-36px">
                             ・Copyrights and Trademarks
-
                         </div>
-
-                        <span className='font-normal font-sans text-[24px] leading-36px'>
-
+                        <span className='font-normal font-DM-Sans text-[12px] sm:text-[24px] leading-36px'>
                             All text, illustrations, logos, photographs, videos, software, and all other information on this
-                            website are the copyrighted property of the Medbank or third parties. <br></br>
+                            website are the copyrighted property of Medbank or third parties. <br />
                             Except for personal use or other use permitted by copyright law, you may not use (including
                             copying, modifying, distributing, or publicly transmitting) any of this information without the
-                            prior permission of the copyright holder. You may not use (including reproduction,
-                            modification, distribution, public transmission, etc.) this information without the prior
-                            permission of the copyright holder. <br></br>
+                            prior permission of the copyright holder. <br />
                             The names of our products and services appearing on our website are our trademarks.
                         </span>
                     </div>
                     <div>
-
-                        <div className="font-sans font-medium text-[24px] leading-36px">
+                        <div className="font-DM-Sans font-medium text-[12px] sm:text-[24px] leading-36px">
                             ・Governing Law and Court of Jurisdiction
-
                         </div>
-                        <span className='font-normal font-sans text-[24px] leading-36px'>
-
-                            Unless otherwise provided herein, the use of this Web site shall be governed by and
-                            construed in accordance with the laws of Japan. <br></br>
+                        <span className='font-normal font-DM-Sans text-[12px] sm:text-[24px] leading-36px'>
+                            Unless otherwise provided herein, the use of this website shall be governed by and
+                            construed in accordance with the laws of Japan. <br />
                             Access to Medbank website is at the discretion of the user, and the user assumes all
-                            responsibility for the use of Medbank website. <br></br>
+                            responsibility for the use of Medbank website. <br />
                             Unless otherwise specified, the Hiroshima District Court shall be the court of first jurisdiction
-                            for all disputes related to the use of this website. <br></br>
+                            for all disputes related to the use of this website. <br />
                         </span>
                     </div>
-                    <span className='font-normal font-sans text-[24px] leading-36px'>
+                    <span className='font-normal font-DM-Sans text-[12px] sm:text-[24px] leading-36px'>
                         【Links to our website】
                         In principle, you are free to link to our website, regardless of whether it is for profit or not.
                         However, please refrain from setting up links that fall under any of the following or may fall
-                        under any of the following. <br></br>
+                        under any of the following. <br />
                         Websites that include content that defames or slanders Medbank, other companies (or
-                        persons), or other organizations, or that is intended to discredit them. <br></br>
+                        persons), or other organizations, or that is intended to discredit them. <br />
                         Websites that infringe or may infringe intellectual property rights such as copyrights,
                         trademarks, property, privacy, portrait rights, or other rights of Medbank, other companies (or
                         persons), or other organizations.
                     </span>
-
-                    <span className='font-normal font-sans text-[24px] leading-36px'>
+                    <span className='font-normal font-DM-Sans text-[12px] sm:text-[24px] leading-36px'>
                         Websites that make it unclear that the contents are those of Medbank website and may
                         cause misunderstanding to third parties, such as when Medbank website is displayed within
-                        a frame. <br></br>
+                        a frame. <br />
                         (Please be sure to set the link in such a way that the screen completely switches to this
-                        website or a new browser window opens to display our website).
+                        website or a new browser window opens to display our website). <br />
                         In addition to the above items, websites that may violate laws, ordinances, regulations
                         including rules and regulations, or public order and morals, or may interfere with the
-                        operation of Medbank website services. <br></br>
+                        operation of Medbank website services. <br />
                         It is not permitted to link to the Website by using registered trademarks such as logos and
                         marks owned by Medbank without permission. It is not permitted to create a link in such a
-                        way as to misidentify the source of the information. <br></br>
-                        Please note that the URL of Medbank website is subject to change without notice. <br></br>
-                        【Others】 <br></br>
+                        way as to misidentify the source of the information. <br />
+                        Please note that the URL of Medbank website is subject to change without notice. <br />
+                        【Others】 <br />
                         Please note that Medbank shall not be liable for any compensation, complaints, etc. related
-                        to the link. <br></br>
-                        ・Prohibited Matters <br></br>
-                        The following actions are prohibited when using this website. <br></br>
+                        to the link. <br />
+                        ・Prohibited Matters <br />
+                        The following actions are prohibited when using this website. <br />
                         ・Actions that cause or may cause annoyance, disadvantage, or damage to other users, third
-                        parties, or Medbank. <br></br>
+                        parties, or Medbank. <br />
                         ・Actions that infringe or may infringe on the property or privacy of other users, third parties,
-                        or Medbank. <br></br>
-                        ・Actions that are or may be offensive to public order and morals. <br></br>
-                        ・Actions that violate or may violate laws, regulations, or ordinances. <br></br>
-                        ・Any other actions that the Company deems inappropriate. <br></br>
+                        or Medbank. <br />
+                        ・Actions that are or may be offensive to public order and morals. <br />
+                        ・Actions that violate or may violate laws, regulations, or ordinances. <br />
+                        ・Any other actions that the Company deems inappropriate. <br />
                     </span>
                 </div>
-
             </section>
-            
-
         </>
     )
 }
 
 export default page
+
