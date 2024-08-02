62616830088b99463bd52a7e389561f66eab151f
Nehapanwal <nehapanwal02@gmail.com>
2024-08-03 01:24:45 +0530

    12th commit

--- app/(LandingPage)/Orderflow/page.jsx
+++ app/(LandingPage)/Orderflow/page.jsx
@@ -20,32 +20,32 @@ const page = () => {
     return (
         <section className='w-full flex flex-col justify-center items-center'>
             <div className='max-w-[1056px] w-full flex flex-col items-start'>
-                <div className='text-[#333333] pt-[60px] pl-[25px]'>
-                    <div className='w-[50px] font-DM-Sans font-medium text-[14px] sm:text-[18px] leading-[24px] pb-[14px]'>
+                <div className='text-[#333333] pt-[24px] md:pt-[60px] pl-[25px]'>
+                    <div className='w-[50px] font-DM-Sans font-medium text-[14px] md:text-[18px] leading-[24px]'>
                         steps
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"1"} subTitle={"Registration"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Please register as a member when you decide to place an order. <br />
                         You can view your order history on My Page.
                         <button className="w-[50px] h-[20px] px-[32px] py-[12px] rounded-[6px] flex items-center justify-center gradient-primary text-[14px] text-white">Sign Up</button>
                     </div>
                     <br />
-                    <div>
+                    <div className='pt-[40px]'>
                         <Image src={Orderflow1} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"2"} subTitle={"Enter your information in My Page"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Please click the “Setup” button on “My page” and enter detailed information about your
                         research institute.
                     </div>
@@ -55,67 +55,67 @@ const page = () => {
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"3"} subTitle={"Create a new order page"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Click the New Order button on My page to create a new order page and follow the steps to
                         place an order.
                     </div>
                     <br />
-                    <div>
+                    <div className='pt-[40px]'>
                         <Image src={Orderflow3} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"4"} subTitle={"Create a request sheet"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Download the request sheet from the New Order page, create and upload the request sheet.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow4} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"5"} subTitle={"Confirmation of Quotation"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Once the request sheet has been received without any problems, you will receive a
                         quotation. Please check the quotation.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow5} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"6"} subTitle={"Place a formal order"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         After confirming the quotation, please place a formal order by clicking the “Place a formal
                         order” button on the “New Order” page. After placing a formal order, the formal order will be
                         automatically accepted by Medbank.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow6} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"7"} subTitle={"Sending samples"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         When you are ready to send samples, please prepare samples and request sheets (our
                         form) and send them to the address below. Please refer to the page of each analysis service
                         you request for the required sample volume. For packing instructions, please refer to this page. <br /> <br />
@@ -126,95 +126,95 @@ const page = () => {
                         3-7-12 Shoji, Ikuno-ku, Osaka, 544-0002, Japan <br />
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow7} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"8"} subTitle={"Quality Check"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Medbank will perform a quality check. The results of the quality check will be sent to you via Mypage. <br />
                         If there is no problem, please proceed to the next step. If there is a problem and you wish to <br />
                         resend or dispose of the sample, please contact us via chat.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow8} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"9"} subTitle={"Library Adjustment"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         Library adjustment will be performed by Medbank. <br />
                         If there is no problem, please proceed to the next step. If there is a problem and you wish to <br />
                         resend or dispose of the sample, please contact us via chat.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow9} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"10"} subTitle={"Inspection and Analysis"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         After the quality check and library adjustment are completed, Medbank will perform the
                         analysis. Mypage will be notified of the start and end of the analysis.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow10} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"11"} subTitle={"Sending of original data and analysis specifications"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         After the analysis is completed, the original data and analysis specifications will be available
                         for download from Mypage. <br />
                         The original data will be shared in the cloud and will be stored for 3 months.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow11} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"12"} subTitle={"Sending of invoice"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         After the original data and analysis specifications have been sent, an invoice will be
                         available for download from Mypage. Please make a bank transfer after confirming the
                         invoice.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow12} />
                     </div>
                 </div>
 
-                <div className='text-[#333333] pt-[20px]'>
-                    <div className='flex flex-col pb-[6px] text-[18px] sm:text-[28px]'>
+                <div className='text-[#333333] pt-[40px]'>
+                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
                         <HomePageSectionHeader2 title={"13"} subTitle={"Sending of Receipt"} />
                     </div>
-                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] sm:text-[24px] leading-[34px] pl-[60px]'>
+                    <div className='max-w-[996.45px] w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
                         You will be able to download the receipt after confirming the transfer of funds. <br />
                         Please note that it may take some time to confirm the transfer of funds.
                     </div>
                     <br />
-                    <div>
+                    <div className=' pt-[40px]'>
                         <Image src={Orderflow13} />
                     </div>
                 </div>
--- app/(LandingPage)/strength/page.jsx
+++ app/(LandingPage)/strength/page.jsx
@@ -8,15 +8,15 @@ import strength5 from '../../../public/Images/Home/strength5.png'
 export default function StrengthPage() {
   return (
     <>
-      <section className="w-full flex flex-col items-center justify-center text-[#333333] gap-[24px] sm:gap-[70px] md:gap-[100px] px-4 lg:px-0 pt-[20px] sm:pt-[60px] ">
-        <div className="w-full lg:w-[1057px] flex flex-col items-start gap-[6px] sm:gap-[10px] md:gap-[24px]">
+      <section className="w-full flex flex-col items-center justify-center text-[#333333] gap-[24px] sm:gap-[70px] lg:gap-[100px] px-4 lg:px-0 pt-[20px] sm:pt-[60px] ">
+        <div className="w-full lg:w-[1057px] flex flex-col items-start gap-[6px] sm:gap-[10px] lg:gap-[24px]">
           <div className='flex flex-col gap-[6px] sm:gap-[20px]'>
             <h2 className="w-full lg:w-[110px] leading-6 font-DM-Sans text-[14px] sm:text-[18px] font-medium">Our Strength</h2>
-            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] md:text-[32px] font-bold leading-8 lg:leading-10">
+            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] lg:text-[32px] font-bold leading-8 lg:leading-10">
               Comprehensive Next Generation Sequencing Processes
             </div>
           </div>
-          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px]">
+          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px]">
             At Medbank, we have the capability to conduct all Next Generation Sequencing
             (NGS) processes from sample preparation to data analysis in our
             state-of-the-art laboratory facilities. Our team of specialized staff is
@@ -27,7 +27,7 @@ export default function StrengthPage() {
             Benefits
           </span>
           <div>
-            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px] leading-8'>
+            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px] leading-8'>
               <li>1. Streamlined and integrated NGS workflow for efficiency and consistency.</li>
               <li>2. Expert guidance and support throughout the entire sequencing process.</li>
               <li>3. Customized solutions tailored to meet the specific needs of each research project.</li>
@@ -37,14 +37,14 @@ export default function StrengthPage() {
         <div className='w-full lg:w-[1057px] flex items-start justify-start'>
           <Image src={strength1} alt='strength1' className='w-full' />
         </div>
-        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] md:gap-[24px]">
+        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] lg:gap-[24px]">
           <div className='flex flex-col gap-[20px] sm:gap-[6px]'>
             <h2 className="w-full lg:w-[110px] leading-6 font-DM-Sans text-[14px] sm:text-[18px] font-medium">Our Strength</h2>
-            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] md:text-[32px] font-bold leading-8 lg:leading-10">
+            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] lg:text-[32px] font-bold leading-8 lg:leading-10">
               Specialized Environment for DNA/RNA Analysis
             </div>
           </div>
-          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px]">
+          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px]">
             To ensure the integrity and reliability of our genetic analysis, Medbank maintains a specialized environment for DNA and
             RNA analysis. Our laboratory facilities are equipped
             with advanced technologies and adhere to strict quality control measures to minimize contamination and ensure the accuracy of our results.
@@ -53,7 +53,7 @@ export default function StrengthPage() {
             Benefits
           </span>
           <div>
-            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px] leading-8'>
+            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px] leading-8'>
               <li>1. Clean and controlled laboratory environment for sensitive molecular analysis.</li>
               <li>2. Minimized risk of contamination and sample degradation.</li>
               <li>3. Consistent and reliable results for high-quality genetic analysis.</li>
@@ -64,14 +64,14 @@ export default function StrengthPage() {
           <Image src={strength2} alt='strength2' className='w-full' />
         </div>
 
-        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] md:gap-[24px]">
+        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] lg:gap-[24px]">
           <div className='flex flex-col gap-[20px] sm:gap-[6px]'>
             <h2 className="w-full lg:w-[110px] leading-6 font-DM-Sans text-[14px] sm:text-[18px] font-medium">Our Strength</h2>
-            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] md:text-[32px] font-bold leading-8 lg:leading-10">
+            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] lg:text-[32px] font-bold leading-8 lg:leading-10">
               Customer-Centric Approach to Analysis Results
             </div>
           </div>
-          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px]">
+          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px]">
             At Medbank, we prioritize the needs and preferences of our clients when delivering analysis results. We provide comprehensive
             support and guidance on data handling and analysis
             methods, ensuring that results are presented in an easy-to-understand format tailored to the specific requirements of each client.
@@ -80,7 +80,7 @@ export default function StrengthPage() {
             Benefits
           </span>
           <div>
-            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px] leading-8'>
+            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px] leading-8'>
               <li>1. Personalized support and guidance throughout the analysis process.</li>
               <li>2. Clear and concise presentation of analysis results for enhanced understanding.</li>
               <li>3. Empowerment of clients with actionable insights for their research projects</li>
@@ -91,14 +91,14 @@ export default function StrengthPage() {
           <Image src={strength3} alt='strength3' className='w-full' />
         </div>
 
-        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] md:gap-[24px]">
+        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] lg:gap-[24px]">
           <div className='flex flex-col gap-[20px] sm:gap-[6px]'>
             <h2 className="w-full lg:w-[110px] leading-6 font-DM-Sans text-[14px] sm:text-[18px] font-medium">Our Strength</h2>
-            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] md:text-[32px] font-bold leading-8 lg:leading-10">
+            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] lg:text-[32px] font-bold leading-8 lg:leading-10">
               Automation of Processes
             </div>
           </div>
-          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px]">
+          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px]">
             We have implemented automation technologies for DNA and RNA extraction, purification, and library preparation, streamlining and
             standardizing these critical processes.
             By automating repetitive tasks, we ensure accuracy, reproducibility, and scalability, enabling us to deliver high-quality results efficiently.
@@ -107,7 +107,7 @@ export default function StrengthPage() {
             Benefits
           </span>
           <div>
-            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px] leading-8'>
+            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px] leading-8'>
               <li>1. Reduced risk of human error and variability.</li>
               <li>2. Increased throughput and faster turnaround times.</li>
               <li>3. Consistent and reliable results across experiments.</li>
@@ -118,14 +118,14 @@ export default function StrengthPage() {
           <Image src={strength4} alt='strength4' className='w-full' />
         </div>
 
-        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] md:gap-[24px]">
+        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] sm:gap-[10px] lg:gap-[24px]">
           <div className='flex flex-col gap-[20px] sm:gap-[6px]'>
             <h2 className="w-full lg:w-[110px] leading-6 font-DM-Sans text-[14px] sm:text-[18px] font-medium">Our Strength</h2>
-            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] md:text-[32px] font-bold leading-8 lg:leading-10">
+            <div className="w-full lg:w-[864px] text-[16px] sm:text-[28px] lg:text-[32px] font-bold leading-8 lg:leading-10">
               State-of-the-Art NGS Platform
             </div>
           </div>
-          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px]">
+          <div className="font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px]">
             Medbank utilizes the latest Next Generation Sequencing (NGS) platforms, including the AVITI system from Element Biosciences.
             This advanced platform offers high precision, rapid sequencing, and cost-effectiveness, allowing us to deliver accurate and timely results to our clients.
           </div>
@@ -133,7 +133,7 @@ export default function StrengthPage() {
             Benefits
           </span>
           <div>
-            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] md:text-[24px] leading-8'>
+            <ol className='font-DM-Sans font-normal text-[12px] sm:text-[18px] lg:text-[24px] leading-8'>
               <li>1. High-quality sequencing data with improved accuracy and coverage.</li>
               <li>2. Reduced sequencing costs and shorter project timelines.</li>
               <li>3. Access to cutting-edge technologies for innovative research applications.</li>
