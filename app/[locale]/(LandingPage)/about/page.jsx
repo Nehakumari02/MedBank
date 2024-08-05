5d4122a39750b0af3d887851ec1af7a7e5902964
Nehapanwal <nehapanwal02@gmail.com>
2024-08-06 02:34:26 +0530

    about page

--- app/[locale]/(LandingPage)/about/page.jsx
+++ app/[locale]/(LandingPage)/about/page.jsx
@@ -24,10 +24,10 @@ export default function AboutPage() {
               Company Philosophy
             </div>
           </div>
-          <div className="font-DM-Sans font-normal text-[12px] md:text-[18px] lg:text-[24px] ">
+          <div className="font-DM-Sans font-normal text-[12px] md:text-[18px] lg:text-[24px]  leading-[20px]">
             &quot;At Medbank, our philosophy is centered around delivering accurate, reliable, and innovative genetic analysis solutions to researchers worldwide. We are dedicated to advancing scientific knowledge and empowering our clients with valuable insights to drive discoveries and advancements in various fields.&quot&quot;
           </div>
-          <span className='font-DM-Sans font-medium text-[16px] md:text-[28px] '>
+          <span className='font-DM-Sans font-medium text-[16px] md:text-[28px] pt-[24px]'>
             Key Principles
           </span>
           <div>
@@ -54,13 +54,13 @@ export default function AboutPage() {
         </div>
 
 
-        <div className="w-full lg:w-[1057px]   flex flex-col items-start gap-[6px] md:gap-[10px] lg:gap-[24px]">
-          <div className='flex flex-col gap-[20px] md:gap-[6px]'>
+        <div className="w-full lg:w-[1057px]   flex flex-col items-start  gap-[24px]">
+         
 
             <div className="w-full lg:w-[864px] text-[16px] md:text-[28px] lg:text-[32px] font-bold leading-8 lg:leading-10">
               GOAL in genetic analysis of Medbank
             </div>
-          </div>
+          
           <div className="font-DM-Sans font-normal text-[12px] md:text-[18px] lg:text-[24px]">
             At Medbank, our overarching goal in genetic analysis is to become a world-leading hub for genetic research and analysis. We aim to contribute significantly to scientific advancements and discoveries by providing state-of-the-art genetic analysis services, driving innovation, and fostering collaborations.
           </div>
@@ -71,43 +71,44 @@ export default function AboutPage() {
 
 
 
-      <div className="flex pt-[100px]">
+      <div className="flex md:flex-row flex-col-reverse pt-[24px] md:pt-[100px]">
 
-        <div className="grid grid-cols-1  gap-6 mb-8 pr-[68px] ">
-          <Image src={About3} alt="Lab Image" className="rounded" />
+        <div className="grid grid-cols-1  gap-6 mb-8 pr-[68px]  place-items-center">
+          <Image src={About3} alt="Lab Image" className="rounded " />
           <Image src={About4} alt="Building Image" className="rounded" />
         </div>
-        <div className=' pt-[60px]'>
-          <HomePageSectionHeader title={"Details"} subTitle={"Details"} />
-          <div className='pl-[62px] pt-[24px]'>
-            <div className="text-4xl font-DM-Sans ">COMPANY DETAILS</div>
-            <div className='text-2xl flex flex-col gap-[8px] pt-[24px]'>
+        <div className='pt-[30px] md:pt-[60px]'>
+          <HomePageSectionHeader title={"Details"} subTitle={"Company Details"} />
+          <div className='pl-[62px] pt-[12px] md:pt-[24px]'>
+            <div className="text-2xl md:text-4xl font-DM-Sans font-medium  gradient-primary bg-clip-text text-transparent">COMPANY DETAILS</div>
+            
+            <div className='text-2xl flex flex-col gap-[6px] md:gap-[8px] pt-[6px] md:pt-[24px]'>
               <p className='font-normal p-0 m-0'><span className='font-medium'>Name:</span> Medbank Pte. Ltd.</p>
               <p className='font-normal p-0 m-0'><span className='font-medium'>Establishment:</span> 15 OCT 2021</p>
               <p className='font-normal p-0 m-0'><span className='font-medium'>Representative employee:</span> Yuki Okada (Doctor)</p>
             </div>
 
-            <div className='pt-[24px] flex flex-col gap-[48px]'>
-              <div className="text-[32px] font-medium">Address</div>
+            <div className='pt-[24px] flex flex-col gap-[24px] md:gap-[48px] '>
+              <div className="text-xs md:text-[32px] font-medium">Address</div>
 
-              <div className="text-2xl font-normal">
+              <div className="text-xs md:text-2xl font-normal leading-[24px]">
                 <p className='font-medium p-0 m-0'> Main Office:</p>
                 <p className='p-0 m-0'>11 Mandalay Road #16-01</p>
                 <p className='p-0 m-0'>Singapore 308232</p>
               </div>
               <hr></hr>
-              <div className="">
+              <div className="text-xs sm:text-2xl leading-[24px]">
                 <p className='font-medium p-0 m-0'>Laboratory in Singapore:</p>
                 <p className='p-0 m-0'>11 Mandalay Road #16-01</p>
                 <p className='p-0 m-0'>Singapore 308232</p>
               </div>
-              <hr/>
-              <div className="">
+              <hr />
+              <div className="text-xs sm:text-2xl leading-[24px]">
                 <p className='font-medium p-0 m-0'>Laboratory in Japan:</p>
                 <p className='p-0 m-0'>〒540-0002</p>
                 <p className='p-0 m-0'>3-7-12 Shoji, Ikuno-ku, Osaka-shi, Osaka Japan</p>
               </div>
-              <hr/>
+              <hr />
 
 
             </div>
@@ -118,88 +119,88 @@ export default function AboutPage() {
 
       <section className='pl-[38px] pr-[24px] md:pl-[156px]  lg:pl-[236px]   md:pr-[95px] w-full flex flex-col items-center justify-center text-[#333333]'>
 
-        <div className="relative max-w-screen-lg w-full lg:w-[1057px] flex flex-col items-start gap-[6px] md:gap-[10px] lg:gap-[32px] pt-[20px] md:pt-[40px] lg:pt-[60px] ">
-          <div className='font-DM-Sans text-[32px] font-bold'>
+        <div className="relative max-w-screen-lg w-full lg:w-[1057px] flex flex-col items-start gap-[32px] pt-[40px] md:pt-[60px] ">
+          <div className='font-DM-Sans text-[18px] md:text-[32px] font-bold'>
             Our Project
           </div>
-          <div className='font-DM-Sans font-normal text-2xl'>
+          <div className='font-DM-Sans font-normal text-[12px] md:text-2xl'>
             Genetic analysis service <br></br>
             Genetic testing (tests related to evidence-based medicine, not DTC) <br></br>
             Ultra-early detection of cancer
           </div>
         </div>
       </section>
- 
-      <section className="md:flex items-center justify-center w-full md:h-[672px] pt-[100px]">
-    <div className='md:w-[70%] h-full flex items-center justify-start text-[#333333]'>
-      <div className='w-full md:pl-[42px] lg:pl-[62px] pr-[20px] flex flex-col gap-[6px] md:gap-[32px]'>
-        <HomePageSectionHeader title={"Analysis"} subTitle={"Inspection and analysis"}></HomePageSectionHeader>
-        <div className='px-[31px] md:pl-[50px] lg:pl-[110px] flex flex-col items-start justify-start gap-[6px] md:gap-[24px]'>
-        <span className="flex items-center font-DM-Sans text-[18px] md:text-[36px] font-medium leading-[24px] md:leading-[42px] gradient-primary bg-clip-text text-transparent">Inspection And Analysis</span>
-        <span className="font-DM-Sans font-normal text-[12px] md:text-[20px] leading-[20px] md:leading-[34px]">
-        The Inspection and Analysis section provides customers with detailed insights into Medbank&apos;s testing and analysis procedures, highlighting the laboratory&apos;s capabilities, equipment, and strengths in genetic analysis.
-        </span>
-       
-      </div>
-      </div>
-    </div>
-    <div className='md:w-[30%] md:h-full px-[31px] md:px-[0px] flex items-center justify-center'>
-        <Image src={About5} alt='About5' className='h-[188px] w-[307px] object-cover md:h-full md:w-full rounded-md md:rounded-none'></Image>
-    </div>
-  </section>
+
+      <section className="md:flex items-center justify-center w-full md:h-[672px] pt-[40px] md:pt-[100px]">
+        <div className='md:w-[70%] h-full flex items-center justify-start text-[#333333]'>
+          <div className='w-full md:pl-[42px] lg:pl-[62px] pr-[20px] flex flex-col gap-[6px] md:gap-[32px]'>
+            <HomePageSectionHeader title={"Analysis"} subTitle={"Inspection and analysis"}></HomePageSectionHeader>
+            <div className='px-[31px] md:pl-[50px] lg:pl-[110px] flex flex-col items-start justify-start gap-[6px] md:gap-[24px]'>
+              <span className="flex items-center font-DM-Sans text-[18px] md:text-[36px] font-medium leading-[24px] md:leading-[42px] gradient-primary bg-clip-text text-transparent">Inspection And Analysis</span>
+              <span className="font-DM-Sans font-normal text-[12px] md:text-[20px] leading-[20px] md:leading-[34px]">
+                The Inspection and Analysis section provides customers with detailed insights into Medbank&apos;s testing and analysis procedures, highlighting the laboratory&apos;s capabilities, equipment, and strengths in genetic analysis.
+              </span>
+
+            </div>
+          </div>
+        </div>
+        <div className='md:w-[30%] md:h-full  flex items-center justify-center py-[20px] md:py-[0px]'>
+          <Image src={About5} alt='About5' className='h-[188px] w-[307px] object-cover md:h-full md:w-full rounded-md md:rounded-none'></Image>
+        </div>
+      </section>
 
 
-  <section className='pl-[38px] pr-[24px] md:pl-[156px] md:pr-[95px] pt-[100px] w-full flex flex-col justify-center items-center font-DM-Sans'>
-            <div className='max-w-[1056px] w-full flex flex-col items-start'>
-                <div className='text-[#333333] pt-[18px] ` pl-[9px] md:pl-[25px]'>
-  
-                    <div className='font-DM-Sans font-bold text-[16px] md:text-[32px] leading-[24px] md:leading-[40px]'>
-                        Our Machines
-                    </div>
-                    <div className='font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pt-[6px] md:pt-[24px]'>
-                    Medbank utilizes state-of-the-art machines and equipment for genetic analysis testing, ensuring accurate and reliable results.
-                    </div>
-                </div>
+      <section className='pl-[38px] pr-[24px] md:pl-[156px] md:pr-[95px]  pt-[12px] md:pt-[100px] w-full flex flex-col justify-center items-center font-DM-Sans'>
+        <div className='max-w-[1056px] w-full flex flex-col items-start'>
+          <div className='text-[#333333]  pl-[9px] md:pl-[25px]'>
 
-                <div className='text-[#333333] pt-[32px] '>
-                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
-                        <HomePageSectionHeader2
-                            title="1"
-                            subTitle="AVITI"
-                        />
-                    </div>
-                    <div className='max-w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
-                    We have introduced a new next-generation sequencer &apos;AVITI&apos; from Element Biosciences, which is capable of developing highly accurate and efficient analyses. AVITI&apos; employs rolling circle amplification (RCA) to minimize amplification errors, and the use of the latest reagents enables high quality (Q50) sequence decoding. 
-                    In addition, &apos;AVITI&apos; has better running costs than conventional products, enabling lower costs and shorter delivery times.
-                    </div>
-                </div>
+            <div className='font-DM-Sans font-bold text-[16px] md:text-[32px] leading-[24px] md:leading-[40px]'>
+              Our Machines
+            </div>
+            <div className='font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pt-[6px] md:pt-[24px]'>
+              Medbank utilizes state-of-the-art machines and equipment for genetic analysis testing, ensuring accurate and reliable results.
+            </div>
+          </div>
 
-                <div className='text-[#333333] pt-[24px] '>
-                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
-                        <HomePageSectionHeader2
-                            title="2"
-                            subTitle="2 Illumina MiSeq"
-                        />
-                    </div>
-                    <div className='max-w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
-                    The Illumina MiSeq platform is employed for smaller-scale sequencing projects, providing accurate and reliable results with fast turnaround times.
-                    </div>
-                </div>
+          <div className='text-[#333333] pt-[32px] '>
+            <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
+              <HomePageSectionHeader2
+                title="1"
+                subTitle="AVITI"
+              />
+            </div>
+            <div className='max-w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
+              We have introduced a new next-generation sequencer &apos;AVITI&apos; from Element Biosciences, which is capable of developing highly accurate and efficient analyses. AVITI&apos; employs rolling circle amplification (RCA) to minimize amplification errors, and the use of the latest reagents enables high quality (Q50) sequence decoding.
+              In addition, &apos;AVITI&apos; has better running costs than conventional products, enabling lower costs and shorter delivery times.
+            </div>
+          </div>
 
-                <div className='text-[#333333] pt-[24px] '>
-                    <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
-                        <HomePageSectionHeader2
-                            title="3"
-                            subTitle="Illumina NextSeq X Plus"
-                        />
-                    </div>
-                    <div className='max-w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
-                    Medbank also plans to begin Novaseq X plus analysis on an outsourced basis in 2025. 
-                    This platform offers scalability, flexibility, and exceptional data quality, making it suitable for a wide range of research projects.
-                    </div>
-                </div>
-              </div>
-              </section>
+          <div className='text-[#333333] pt-[24px] '>
+            <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
+              <HomePageSectionHeader2
+                title="2"
+                subTitle="2 Illumina MiSeq"
+              />
+            </div>
+            <div className='max-w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
+              The Illumina MiSeq platform is employed for smaller-scale sequencing projects, providing accurate and reliable results with fast turnaround times.
+            </div>
+          </div>
+
+          <div className='text-[#333333] pt-[24px] '>
+            <div className='flex flex-col pb-[6px] text-[18px] md:text-[28px]'>
+              <HomePageSectionHeader2
+                title="3"
+                subTitle="Illumina NextSeq X Plus"
+              />
+            </div>
+            <div className='max-w-full font-DM-Sans font-normal text-[12px] md:text-[24px] leading-[34px] pl-[60px]'>
+              Medbank also plans to begin Novaseq X plus analysis on an outsourced basis in 2025.
+              This platform offers scalability, flexibility, and exceptional data quality, making it suitable for a wide range of research projects.
+            </div>
+          </div>
+        </div>
+      </section>
 
 
     </>
