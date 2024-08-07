dc26745a65faa3ee2ed9f667a408c0266b5f2e69
nehakumari02 <b20115@students.iitmandi.ac.in>
2024-08-07 19:05:08 +0530

    homepage Nav section

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,14 +1,9 @@
-
 import { ReactNode } from 'react';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
-
-// Import any necessary styles and components
 import './globals.css';
 import { cn } from '../../lib/utils';
 import { DMSans } from '../../lib/fonts';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 
 export default async function LocaleLayout({
   children,
@@ -17,16 +12,13 @@ export default async function LocaleLayout({
   children: ReactNode;
   params: { locale: string };
 }) {
-  // Fetch messages for the current locale
   const messages = await getMessages(locale as any);
 
   return (
     <html lang={locale}>
       <body className={cn(DMSans.variable, "text-black")}>
         <NextIntlClientProvider messages={messages}>
-          <Header />
           <main>{children}</main>
-          <Footer />
         </NextIntlClientProvider>
       </body>
     </html>
