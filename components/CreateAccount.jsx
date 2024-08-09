83a2c800b3acecc53b462dd90cd60935e379737c
nehakumari02 <b20115@students.iitmandi.ac.in>
2024-08-09 20:47:26 +0530

    addition in home page price table

--- components/CreateAccount.jsx
+++ components/CreateAccount.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import HomePageSectionHeader from './HomePageSectionHeader';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const CreateAccount = () => {
+  const language = usePathname().split("/")[1];
   const t= useTranslations("HomePage.CreateAccount");
   return (
     <div className="hidden md:block md:px-[42px] lg:px-[62px] pt-[100px]">
@@ -14,8 +17,8 @@ const CreateAccount = () => {
               {t("description")}
             </p>
             <div className='flex gap-[12px]'>
-            <button className="h-[40px] p-[1px] rounded-[6px] flex items-center justify-center gradient-primary border-[#60B7CF]"><div className='bg-white w-full h-full px-[32px] py-[12px] rounded-[5px] flex items-center justify-center'><span className='gradient-primary bg-clip-text text-transparent'>Sign In</span></div></button>
-            <button className="h-[40px] px-[32px] py-[12px] rounded-[6px] flex items-center justify-center gradient-primary text-white">Sign Up</button>
+            <Link href={`/${language}/signin`} className="h-[40px] p-[1px] rounded-[6px] flex items-center justify-center gradient-primary border-[#60B7CF]"><div className='bg-white w-full h-full px-[32px] py-[12px] rounded-[5px] flex items-center justify-center'><span className='gradient-primary bg-clip-text text-transparent'>Sign In</span></div></Link>
+            <Link href={`/${language}/Registration`} className="h-[40px] px-[32px] py-[12px] rounded-[6px] flex items-center justify-center gradient-primary text-white">Sign Up</Link>
             </div>
 
             <div className="w-full flex items-center justify-center">
