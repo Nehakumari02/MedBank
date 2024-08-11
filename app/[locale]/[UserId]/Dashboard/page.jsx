848af6c207748cdec3502156c167462faedc3d20
nehakumari02 <b20115@students.iitmandi.ac.in>
2024-08-11 20:14:00 +0530

    settings page ui

--- app/[locale]/[UserId]/Dashboard/page.jsx
+++ app/[locale]/[UserId]/Dashboard/page.jsx
@@ -1,5 +1,17 @@
+"use client"
 import React from 'react'
 import OrderOverView from '../../../../components/UserDashboard/Dashboard/OrderOverView'
+import DashboardDataTable from '../../../../components/UserDashboard/Dashboard/DataTable'
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const Dashboard = () => {
   const orderOverview ={
@@ -11,7 +23,23 @@ const Dashboard = () => {
   return (
     <div className='w-full p-[19px]'>
       <OrderOverView orderOverview={orderOverview}/>
+      {/* <DashboardDataTable/> */}
 
+      <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
     </div>
   )
 }
